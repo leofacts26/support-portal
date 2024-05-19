fed317a8c2a6f3d367c212598593ba3476df83cf
prasankumar26 <prasanthedeveloper@gmail.com>
2024-05-19 14:21:21 +0530

    explore india

--- src/pages/homepage/ExploreIndia.jsx
+++ src/pages/homepage/ExploreIndia.jsx
@@ -1,9 +1,141 @@
-import React from 'react'
+import React, { useState } from 'react'
+import DataTable from 'react-data-table-component';
+
+const rows = [
+  {
+    personID: 1,
+    fullName: "Mumbai",
+    image: "https://dashkit.goodthemes.co/assets/img/avatars/profiles/avatar-1.jpg",
+  },
+  {
+    personID: 2,
+    fullName: "Bangalore",
+    image: "https://dashkit.goodthemes.co/assets/img/avatars/profiles/avatar-1.jpg",
+  },
+  {
+    personID: 3,
+    fullName: "Chennai",
+    image: "https://dashkit.goodthemes.co/assets/img/avatars/profiles/avatar-1.jpg",
+  },
+  {
+    personID: 4,
+    fullName: "Hyderabad",
+    image: "https://dashkit.goodthemes.co/assets/img/avatars/profiles/avatar-1.jpg",
+  },
+  {
+    personID: 5,
+    fullName: "Pune",
+    image: "https://dashkit.goodthemes.co/assets/img/avatars/profiles/avatar-1.jpg",
+  },
+  {
+    personID: 6,
+    fullName: "Gurgaon",
+    image: "https://dashkit.goodthemes.co/assets/img/avatars/profiles/avatar-1.jpg",
+  },
+  {
+    personID: 7,
+    fullName: "Ranchi",
+    image: "https://dashkit.goodthemes.co/assets/img/avatars/profiles/avatar-1.jpg",
+  },
+  {
+    personID: 8,
+    fullName: "Kolkata",
+    image: "https://dashkit.goodthemes.co/assets/img/avatars/profiles/avatar-1.jpg",
+  },
+  {
+    personID: 9,
+    fullName: "Goa",
+    image: "https://dashkit.goodthemes.co/assets/img/avatars/profiles/avatar-1.jpg",
+  },
+  {
+    personID: 10,
+    fullName: "Madurai",
+    image: "https://dashkit.goodthemes.co/assets/img/avatars/profiles/avatar-1.jpg",
+  },
+  {
+    personID: 11,
+    fullName: "Coiambator",
+    image: "https://dashkit.goodthemes.co/assets/img/avatars/profiles/avatar-1.jpg",
+  },
+];
 
 const ExploreIndia = () => {
+  const [data, setData] = useState(rows);
+
+  const handleSearch = (e) => {
+    const searchValue = e.target.value.toLowerCase();
+    const newRows = rows.filter((row) => {
+      return (
+        row.personID.toString().toLowerCase().includes(searchValue) ||
+        row.fullName.toLowerCase().includes(searchValue)
+      );
+    });
+    setData(newRows);
+  };
+
+  const columns = [
+    {
+      name: "ID",
+      selector: row => row.personID,
+      sortable: true,
+    },
+    {
+      name: "City Name",
+      selector: row => row.fullName,
+      sortable: true,
+    },
+    {
+      name: "Image Preview",
+      cell: row => (
+        <a href={row.image} target="_blank" rel="noopener noreferrer">
+          Image
+        </a>
+      ),
+      sortable: false,
+    },
+    {
+      name: "Action",
+      cell: (row) => (
+        <>
+          <span className='text-primary cursor-pointer' onClick={() => handleEdit(row.personID)}>Edit / </span>
+          <span className='text-primary cursor-pointer' onClick={() => handleDelete(row.personID)}> {" "} Delete </span>
+        </>
+      ),
+      ignoreRowClick: true,
+      allowOverflow: true,
+      button: true,
+    },
+  ];
+
+  const handleEdit = (event) => {
+    console.log(event, "event");
+  }
+  const handleDelete = (event) => {
+    console.log(event, "event");
+  }
+
   return (
     <>
-      <h2>Explore</h2>
+      <div className="container my-5">
+        <div className="card">
+          <input
+            type="search"
+            className="form-control-sm border ps-3 py-3"
+            placeholder="Search"
+            onChange={handleSearch}
+          />
+          <DataTable
+            columns={columns}
+            data={data}
+            fixedHeader
+            pagination
+            selectableRows
+          // title="React-Data-Table-Component Tutorial."
+          />
+        </div>
+      </div>
+
+      <br />
     </>
   )
 }
